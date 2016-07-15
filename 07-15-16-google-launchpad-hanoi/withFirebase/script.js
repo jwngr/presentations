3fde3785e8b26a458cf57ab79db8027c1870e302
jwngr <jacob@firebase.com>
2016-07-15 08:14:59 +0700

    Finished up Launchpad Hanoi code

--- 07-15-16-google-launchpad-hanoi/withFirebase/script.js
+++ 07-15-16-google-launchpad-hanoi/withFirebase/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   // Initialize the Firebase SDK
   var config = {
-    apiKey: "AIzaSyABFtaPbOJ2k4TWe2TguEkHDG80ZGYDGSs",
-    authDomain: "gfm-chat.firebaseapp.com",
-    databaseURL: "https://gfm-chat.firebaseio.com",
-    storageBucket: "gfm-chat.appspot.com"
+    apiKey: "AIzaSyD9wIdkMgn-hPR8KgBO6iNcnsSRqh8ae2g",
+    authDomain: "launchpad-hanoi.firebaseapp.com",
+    databaseURL: "https://launchpad-hanoi.firebaseio.com",
+    storageBucket: "launchpad-hanoi.appspot.com"
   };
   firebase.initializeApp(config);
 
@@ -13,28 +13,30 @@ $(document).ready(function() {
   var storageRef = firebase.storage().ref("messages");
 
   // Get references to DOM elements
-  var $usernameInput = $("#username");
   var $newMessageInput = $("#newMessage");
   var $newFileInput = $("#newFile");
   var $messageList = $("#messageList");
   var $submitButton = $("#submitButton");
+  var $loginButton = $("#loginButton");
+  var $logoutButton = $("#logoutButton");
+  var $loggedInName = $("#loggedInName");
 
   // Adds a new message to the message list
-  function addMessageToHtml(username, text, imageURL) {
+  function addMessageToHtml(name, text, imageURL) {
     var el;
     if (imageURL) {
-      el = $("<li class='list-group-item'><b>" + username + ":</b> " + text + "<img src='" + imageURL + "' /></li>")
+      el = $("<li class='list-group-item'><b>" + name + ":</b> " + text + "<img src='" + imageURL + "' /></li>")
     } else {
-      el = $("<li class='list-group-item'><b>" + username + ":</b> " + text + "</li>")
+      el = $("<li class='list-group-item'><b>" + name + ":</b> " + text + "</li>")
     }
 
     $messageList.append(el);
   }
 
   // Adds a new message to the Firebase Database
-  function addMessageToFirebaseDatabase(newMessageRef, username, text, downloadURL) {
+  function addMessageToFirebaseDatabase(newMessageRef, text, downloadURL) {
     newMessageRef.set({
-      username: username,
+      name: globalFirebaseUser.displayName,
       text: text,
       downloadURL: downloadURL || null
     }).then(function() {
@@ -48,28 +50,62 @@ $(document).ready(function() {
   // Loop through the last ten messages stored in Firebase
   databaseRef.limitToLast(10).on("child_added", function(snapshot) {
     var message = snapshot.val();
-    addMessageToHtml(message.username, message.text, message.downloadURL);
+    addMessageToHtml(message.name, message.text, message.downloadURL);
   });
 
   // Listen for key presses on the new message input
   $submitButton.click(function() {
-    var username = $usernameInput.val().trim();
     var newMessage = $newMessageInput.val().trim();
     var newFile = $newFileInput.prop("files")[0];
 
-    if (username && newMessage) {
+    if (globalFirebaseUser && newMessage) {
       var newMessageRef = databaseRef.push();
 
       if (newFile) {
         storageRef.child(newMessageRef.key).put(newFile).then(function(snapshot) {
           var downloadURL = snapshot.downloadURL;
-          addMessageToFirebaseDatabase(newMessageRef, username, newMessage, downloadURL);
+          addMessageToFirebaseDatabase(newMessageRef, newMessage, downloadURL);
         }).catch(function(error) {
           console.log("Error uploading new file to Firebase Storage:", error);
         });
       } else {
-        addMessageToFirebaseDatabase(newMessageRef, username, newMessage);
+        addMessageToFirebaseDatabase(newMessageRef, newMessage);
       }
     }
+  });
+
+  // Listen for changes in auth state and show the appropriate buttons and messages
+  var globalFirebaseUser;
+  firebase.auth().onAuthStateChanged(function(firebaseUser) {
+    globalFirebaseUser = firebaseUser;
+
+    if (firebaseUser) {
+      // User logged in
+      $loginButton.hide();
+      $logoutButton.show();
+      $loggedInName.text(firebaseUser.displayName);
+      $newMessageInput.prop("disabled", false);
+    } else {
+      // User logged out
+      $loginButton.show();
+      $logoutButton.hide();
+      $loggedInName.text("None");
+      $newMessageInput.prop("disabled", true);
+    }
+  });
+
+  // Login with GitHub when the login button is pressed
+  $loginButton.click(function() {
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(firebaseUser) {
+      console.log("User signed in with GitHub:", firebaseUser);
+    }).catch(function(error) {
+      console.error("Error authenticating with GitHub:", error);
+    });
+  });
+
+  // Logout when the logout button is pressed
+  $logoutButton.click(function() {
+    firebase.auth().signOut();
   });
 });
